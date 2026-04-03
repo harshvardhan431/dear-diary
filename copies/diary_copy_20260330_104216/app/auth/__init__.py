450cd7819a8b8dc218f530577d0cbb12b79bb212
harshvardhan431 <harshvardhansinghchouhan431@gmail.com>
2026-04-03 17:55:14 +0530

    fixed venv

--- copies/diary_copy_20260330_104216/app/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
- #always import routes here so even the decorators are registered
-bp = Blueprint("auth", __name__) #this is a module package always __init__.py should be the entery point 
-#auth is the blueprint name and it acts as an endpoint 
-#only if your template folder is inside this auth or folder then include its name otherwise just leave it from app.auth import routes
-from app.auth import routes
-#always import this line after difining blueprint 
